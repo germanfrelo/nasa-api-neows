f1d9d5b549a7683e33803900fcc01cbf16ee0df9
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-05-29 17:21:10 +0200

    Run Prettier with new changes

--- src/components/Orbital.jsx
+++ src/components/Orbital.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Hazard from './Hazard';
-import Passing from './Passing';
+import React from "react";
+import Hazard from "./Hazard";
+import Passing from "./Passing";
 
 export default function Orbital({
 	name,
@@ -10,16 +10,20 @@ export default function Orbital({
 }) {
 	return (
 		<div
-			className={is_potentially_hazardous_asteroid ? 'is-hazard' : 'no-hazard'}
+			className={is_potentially_hazardous_asteroid ? "is-hazard" : "no-hazard"}
 		>
-			<h2>{name.replace(/[()]/g, '')}</h2>
+			<h2>{name.replace(/[()]/g, "")}</h2>
 			<p>
-				Potentially hazardous?{' '}
+				Potentially hazardous?{" "}
 				<Hazard yes={is_potentially_hazardous_asteroid} />
 			</p>
 			<Passing data={close_approach_data} />
 			<p className="more">
-				<a href={nasa_jpl_url} rel="noreferrer" target="_blank">
+				<a
+					href={nasa_jpl_url}
+					rel="noreferrer"
+					target="_blank"
+				>
 					Find out more
 				</a>
 			</p>
--- src/components/Passing.jsx
+++ src/components/Passing.jsx
@@ -1,22 +1,22 @@
-import format from 'date-fns/format';
-import formatNumber from 'format-number';
-import React from 'react';
+import format from "date-fns/format";
+import formatNumber from "format-number";
+import React from "react";
 
 const formatter = formatNumber({
-	integerSeparator: '\u00A0',
-	decimal: ',',
+	integerSeparator: "\u00A0",
+	decimal: ",",
 	round: 0,
 });
 
 export default function Passing({ data }) {
 	return data.map((_, i) => (
 		<p key={i}>
-			Misses {_.orbiting_body} tomorrow at{' '}
-			{format(_.epoch_date_close_approach, 'HH:mm')} by{' '}
+			Misses {_.orbiting_body} tomorrow at{" "}
+			{format(_.epoch_date_close_approach, "HH:mm")} by{" "}
 			{formatter(_.miss_distance.kilometers)}
-			&nbsp;km whilst travelling at{' '}
+			&nbsp;km whilst travelling at{" "}
 			{formatter(_.relative_velocity.kilometers_per_hour)}
-			{'\u00A0'}
+			{"\u00A0"}
 			km/h
 		</p>
 	));
