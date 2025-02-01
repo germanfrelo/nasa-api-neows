f9d1b73a662c21e8716e4d7949689193312723cd
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-02-01 10:41:40 +0100

    fix(react): replace deprecated `ReactDOM.render` with `createRoot`

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-render(<App />, document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
 
 if (module.hot) {
 	module.hot.accept(function () {
