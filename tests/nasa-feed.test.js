31d843e8babdfc351b20231fb4e3be14fdc9d403
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-02-01 17:50:39 +0100

    refactor: use the Fetch API instead of Axios

--- tests/nasa-feed.test.js
+++ tests/nasa-feed.test.js
@@ -1,46 +1,52 @@
 /* eslint-env node */
-const axios = require('axios');
-const addDays = require('date-fns/addDays');
+
+const addDays = require("date-fns/addDays");
 
 function getDate(d = new Date()) {
-	return d.toJSON().split('T')[0];
+	return d.toJSON().split("T")[0];
 }
 
-// tests the structure of the json is the way we expect
-function test() {
-	return axios
-		.get(
-			`https://api.nasa.gov/neo/rest/v1/feed?start_date=${getDate()}&api_key=DEMO_KEY`
-		)
-		.then(({ data }) => {
-			const day = getDate(addDays(new Date(), 1));
+async function fetchData(url) {
+	const response = await fetch(url);
 
-			// this will also throw if we can't access the value
-			let dayData = null;
-			try {
-				dayData = data.near_earth_objects[day];
-			} catch (e) {
-				throw new Error(
-					'Unexpected data structure, was looking for :root.near_earth_objects[array]'
-				);
-			}
+	if (!response.ok) {
+		throw new Error(`Request failed with status code ${response.status}`);
+	}
 
-			if (!dayData) {
-				throw new Error('Missing any day for tomorrow');
-			}
-
-			const first = dayData[0];
-			if (
-				typeof first.is_potentially_hazardous_asteroid === 'undefined'
-			) {
-				throw new Error(
-					'Missing key "is_potentially_hazardous_asteroid" from first data point, presuming remaining data is wrong.'
-				);
-			}
-		});
+	return response.json();
 }
 
-test().catch((e) => {
-	console.log(`Failed: ${e.message}`);
-	process.exit(1);
-});
+// Tests the structure of the JSON to ensure it matches expectations
+async function test() {
+	const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${getDate()}&api_key=DEMO_KEY`;
+
+	try {
+		const data = await fetchData(url);
+		const day = getDate(addDays(new Date(), 1));
+
+		let dayData = null;
+		try {
+			dayData = data.near_earth_objects[day];
+		} catch (e) {
+			throw new Error(
+				"Unexpected data structure, was looking for :root.near_earth_objects[array]",
+			);
+		}
+
+		if (!dayData) {
+			throw new Error("Missing any day for tomorrow");
+		}
+
+		const first = dayData[0];
+		if (typeof first.is_potentially_hazardous_asteroid === "undefined") {
+			throw new Error(
+				'Missing key "is_potentially_hazardous_asteroid" from first data point, presuming remaining data is wrong.',
+			);
+		}
+	} catch (error) {
+		console.log(`Failed: ${error.message}`);
+		process.exit(1);
+	}
+}
+
+test();
